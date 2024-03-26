6bfe648be8d84d1d4ac6dfa272930bf9d886579b
ambroisegithub <muhayimana21@gmail.com>
2024-03-26 12:09:23 +0200

    deep copy and shallow copies

---
+++ AdvancedChallenges/deep_shallowCopies.js
@@ -0,0 +1,23 @@
+// Shallow copies
+const originalObject = {
+  firstName: "ambroise",
+  lastName: "muhayimana",
+  address: {
+    city: "kigali",
+    country: "Rwanda",
+  },
+};
+
+// in shallow copies the copies objects will affect the original objects(we use assign function to make shallow copies)
+// const shallowCopy = Object.assign({}, originalObject);
+
+// shallowCopy.address.city = "nyagatare";
+// console.log(shallowCopy.address.city);
+// console.log(originalObject.address.city);
+
+//The change in copied does not affect the changes in of original objects
+
+const deepCopy = JSON.parse(JSON.stringify(originalObject));
+deepCopy.address.city = "mukayanza";
+console.log(deepCopy.address.city);
+console.log(originalObject.address.city);
